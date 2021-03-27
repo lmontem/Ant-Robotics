dbe90333809e20845f446e4595de303a5906b8e8
lmontem <lmontem7@gmail.com>
2021-03-27 11:47:29 -0400

    product carosel working

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,66 @@
 import { team } from "../configs/team.js";
 import {gallery} from "../configs/gallery.js";
+import {products} from "../configs/products.js";
+
+//product buttons
+const productNextBtn = document.querySelector('.product__next-btn');
+const productPreviousBtn = document.querySelector('.product__previous-btn');
+const getMoreInfoBtn = document.querySelector('.product__info-btn');
+//product elements
+const productImage = document.querySelector('.product__image');
+const productHeading = document.querySelector('.product__heading');
+
+let productCurrentImage = 0;
+//product next button logic
+productNextBtn.addEventListener('click', ()=>{
+    if(productCurrentImage < products.length -1){        
+        productCurrentImage++;
+        productImage.src = Object.values(products)[productCurrentImage].image;              
+        productHeading.textContent = Object.values(products)[productCurrentImage].heading;
+        document.querySelector('.max-payload').textContent = Object.values(products)[productCurrentImage].maxpayload;
+        document.querySelector('.size').textContent = Object.values(products)[productCurrentImage].size;
+        document.querySelector('.max-speed').textContent = Object.values(products)[productCurrentImage].maxspeed;
+        document.querySelector('.running-time').textContent = Object.values(products)[productCurrentImage].runningtime;
+        document.querySelector('.charging-time').textContent = Object.values(products)[productCurrentImage].chargingtime;
+    }
+    else{
+        productCurrentImage = 0;
+        productImage.src = Object.values(products)[productCurrentImage].image;              
+        productHeading.textContent = Object.values(products)[productCurrentImage].heading;
+        document.querySelector('.max-payload').textContent = Object.values(products)[productCurrentImage].maxpayload;
+        document.querySelector('.size').textContent = Object.values(products)[productCurrentImage].size;
+        document.querySelector('.max-speed').textContent = Object.values(products)[productCurrentImage].maxspeed;
+        document.querySelector('.running-time').textContent = Object.values(products)[productCurrentImage].runningtime;
+        document.querySelector('.charging-time').textContent = Object.values(products)[productCurrentImage].chargingtime;
+    }
+})
+//product previous button logic
+productPreviousBtn.addEventListener('click',()=>{
+    if( productCurrentImage >=1){
+        productCurrentImage--;
+       productImage.src = Object.values(products)[productCurrentImage].image;
+        productHeading.textContent = Object.values(products)[productCurrentImage].heading;
+        document.querySelector('.max-payload').textContent = Object.values(products)[productCurrentImage].maxpayload;
+        document.querySelector('.size').textContent = Object.values(products)[productCurrentImage].size;
+        document.querySelector('.max-speed').textContent = Object.values(products)[productCurrentImage].maxspeed;
+        document.querySelector('.running-time').textContent = Object.values(products)[productCurrentImage].runningtime;
+        document.querySelector('.charging-time').textContent = Object.values(products)[productCurrentImage].chargingtime;
+    }else{
+        productCurrentImage = products.length -1;
+        productImage.src = Object.values(products)[productCurrentImage].image;              
+        productHeading.textContent = Object.values(products)[productCurrentImage].heading;
+        document.querySelector('.max-payload').textContent = Object.values(products)[productCurrentImage].maxpayload;
+        document.querySelector('.size').textContent = Object.values(products)[productCurrentImage].size;
+        document.querySelector('.max-speed').textContent = Object.values(products)[productCurrentImage].maxspeed;
+        document.querySelector('.running-time').textContent = Object.values(products)[productCurrentImage].runningtime;
+        document.querySelector('.charging-time').textContent = Object.values(products)[productCurrentImage].chargingtime;
+    }
+})
 
 //gallery buttons
 const galleryNextBtn = document.querySelector('.gallery__next-btn');
 const galleryPreviousBtn = document.querySelector('.gallery__previous-btn');
+//gallery elements
 const galleryImage = document.querySelector('.gallery__image');
 const galleryCaption = document.querySelector('.gallery__caption');
 //next button logic
@@ -11,6 +68,10 @@ let currentImage = 0;
 galleryNextBtn.addEventListener('click', ()=>{
     if(currentImage < gallery.length -1){        
         currentImage++;
+        galleryImage.style.backgroundImage = `url(${Object.values(gallery)[currentImage].image})`;              
+        galleryCaption.textContent = Object.values(gallery)[currentImage].caption
+    }else{
+        currentImage = 0;
         galleryImage.style.backgroundImage = `url(${Object.values(gallery)[currentImage].image})`;              
         galleryCaption.textContent = Object.values(gallery)[currentImage].caption
     }
@@ -22,7 +83,13 @@ galleryPreviousBtn.addEventListener('click',()=>{
         currentImage--;
         galleryImage.style.backgroundImage = `url(${Object.values(gallery)[currentImage].image})`;              
         galleryCaption.textContent = Object.values(gallery)[currentImage].caption
+    }else{
+        currentImage = gallery.length -1;
+        galleryImage.style.backgroundImage = `url(${Object.values(gallery)[currentImage].image})`;              
+        galleryCaption.textContent = Object.values(gallery)[currentImage].caption
     }
+    
+
 })
 
 //team constants
@@ -48,4 +115,3 @@ function teamMembers(member) {
 team.forEach(item => {
     teamCards.append(teamMembers(item));
 })
-
