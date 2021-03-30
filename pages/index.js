97a6d2acb9a95af0336f10f3ef400579ae442903
DesireeJoy <desireebradish@gmail.com>
2021-03-29 20:58:27 -0400

    Add Map

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,43 @@
-import { products } from "../configs/products.js";
+import { benefits } from "../configs/benefits.js";
+import { custBenefits } from "../configs/custBenefits.js";
 
-const BenefitsBox = document.querySelector(".benefits-box__card-cont");
+const BenefitsBox = document.querySelector(".Benefits");
+const custBenBox = document.querySelector(".custBenefits");
 
 const benTemplate = document
   .querySelector(".benefit-template")
-  .content.querySelector(".team__card");
+  .content.querySelector(".card");
 
 function getBenefits(benefit) {
   const newCard = benTemplate.cloneNode(true);
-  const cardIcon = benTemplate.querySelector(".benefits-box__icon");
-  const cardTitle = benTemplate.querySelector(".card__heading");
-  const cardDesc = benTemplate.querySelector(".card_desc");
-  cardIcon.style.backgroundImage = `${benefit.image}`;
+  const cardIcon = newCard.querySelector(".benefits-box__icon");
+  const cardTitle = newCard.querySelector(".card__heading");
+  const cardDesc = newCard.querySelector(".card__desc");
+  cardIcon.style.backgroundImage = `url(${benefit.image})`;
   cardTitle.textContent = benefit.title;
   cardDesc.textContent = benefit.desc;
   return newCard;
 }
 
-products.forEach((benefit) => {
+benefits.forEach((benefit) => {
   BenefitsBox.append(getBenefits(benefit));
+});
+
+const custBenTemplate = document
+  .querySelector(".cust-benefit-template")
+  .content.querySelector(".card");
+
+function getCustBenefits(benefit) {
+  const newCard = custBenTemplate.cloneNode(true);
+  const cardImage = newCard.querySelector(".benefits-box__image");
+  const cardTitle = newCard.querySelector(".card__heading");
+  const cardDesc = newCard.querySelector(".card__desc");
+  cardImage.style.backgroundImage = `url(${benefit.image})`;
+  cardTitle.textContent = benefit.title;
+  cardDesc.textContent = benefit.desc;
+  return newCard;
+}
+
+custBenefits.forEach((benefit) => {
+  custBenBox.append(getCustBenefits(benefit));
 });
